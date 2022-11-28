f93e7482faef103bd965ad1f13a3b112b720d41d
Brandon Choi <bchoi28@gmail.com>
2022-11-28 14:39:34 -0500

    added set parent

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        if self.parent == nil && node != nil
+            node.children << self
+        elsif self.parent != nil && node != nil
+            self.parent.children.reject!{|child|child == self}
+            node.children << self
+        elsif self.parent != nil && node == nil
+            self.parent.children.reject!{|child|child == self}
+        end
+        @parent = node
+    end
+
+    
+
+
 end
